578ee652474183b10470d8c03be010575ed35023
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-10-27 18:12:21 +0200

    Add dynamic routing

--- routes/users/index.js
+++ routes/users/index.js
@@ -3,15 +3,18 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => {
-	res.send(`<h1>Voici la page de tous les utilisateurs 👨‍👨‍👧‍👦</h1>`)
+	res.send(`
+	<h1>Voici la page de tous les utilisateurs 👨‍👨‍👧‍👦</h1>
+	<h2>${req.query.theme} ${req.query.sort}</h2>
+	`)
 })
 
 router.post("/", (req, res) => {
 	res.send("<h1>Voici la page des utilisateurs 👨‍👨‍👧‍👦 avec la requête POST</h1>")
 })
 
-router.get("/jonathan", (req, res) => {
-	res.send("<h1>Voici la page 📄 de mon profil utilisteur 👨‍👨‍👧‍👦</h1>")
+router.get("/:username", (req, res) => {
+	res.send(`<h1>Voici la page 📄 du profil de ${req.params.username} 👨‍👨‍👧‍👦</h1>`)
 })
 
 module.exports = router
