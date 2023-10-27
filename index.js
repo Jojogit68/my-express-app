036d07ae750010ce810d9120fda805f27f9a211b
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-10-27 16:26:38 +0200

    add any routes

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello world! 🌍')
+app.get("/", (req, res) => {
+	res.send(`<h1>Express : Bienvenue ! 👌</h1>`)
 })
+
+app.get("/users", (req, res) => {
+	res.send(`<h1>Voici la page des utilisateurs 👨‍👨‍👧‍👦</h1>`)
+})
+
+app.post("/users", (req, res) => {
+	res.send("<h1>Voici la page des utilisateurs 👨‍👨‍👧‍👦 avec la requête POST</h1>")
+})
+
+app.all("/any-route", (req, res) => {
+	res.send(
+		"<h1>Cette page 📄 peut être obtenue avec tous les types de requête HTTP</h1>"
+	)
+})
+
+
 
 app.listen(3000)
