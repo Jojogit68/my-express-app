73a08de00737220c4981134dece896e6fbeb0de8
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-10-28 11:31:24 +0200

    Add serving static files with express.static()

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express")
-const users = require("./routes/users/index")
-const posts = require("./routes/posts/index")
+const app = express()
 const path = require("path")
 
-const app = express()
-
-app.use("/users", users)
-app.use("/posts", posts)
+const users = require("./routes/users/index")
+const posts = require("./routes/posts/index")
+// console.log(path.join(__dirname, "public"))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
+
+app.use("/users", users)
+app.use("/posts", posts)
 
 app.use((req, res, next) => {
 	console.log(new Date().toUTCString())
